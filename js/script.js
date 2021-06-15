57cd1b9181574e8b07e18af402d04e9230ea7128
SRHembree <75818806+SRHembree@users.noreply.github.com>
2021-06-14 22:04:10 -0400

    Slowly fixing the code to be bootstrap

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("hamburger"); 
+    if (x.style.display === "block") {
+        x.style.display = "none"; 
+    } else {
+        x.style.display = "block";
+    }
+}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    //Get the button:
+  var mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } 
+  else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTo({ top: 0, behavior: 'smooth' }); // For Safari
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
+}
